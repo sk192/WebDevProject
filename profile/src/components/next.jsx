72c81b1b4c917af2c91039973b4f2f50775f68b3
sk192 <korulkar.shweta7@gmail.com>
2021-11-29 15:33:18 -0600

    next button code

--- profile/src/components/next.jsx
+++ profile/src/components/next.jsx
@@ -1,10 +1,12 @@
 import Button from "@restart/ui/esm/Button";
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Next(props){
     return(
-        <Button onClick={props.toggle} disabled={props.active} className="btn"><FontAwesomeIcon icon="FaAngleLeft" /></Button>
+        <Button onClick={props.toggle} disabled={props.active} className="btn1">
+            <i class="fa fa-angle-right"></i>
+            
+        </Button>
 
     );
 }
