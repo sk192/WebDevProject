6c70444e08a43608656245eb9df1fedad7d90ef6
sk192 <korulkar.shweta7@gmail.com>
2021-11-29 15:32:45 -0600

    Contains information about project work

--- profile/src/components/profile.jsx
+++ profile/src/components/profile.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function Profile(props){
     return(
         <div className="projects">
-            <img src={props.img} alt="images" width="400px" height="400px" />
+            <img src={props.img} alt="images" width="100%" height="400px" />
             <br/>
+            <br />
             <a href={props.link}>Github</a>
             <h1>{props.name}</h1>
         </div>
