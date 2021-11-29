75385a69f987eb68a021b5e0db8708d58e5e2452
sk192 <korulkar.shweta7@gmail.com>
2021-11-29 15:33:33 -0600

    previous button code

--- profile/src/components/previous.jsx
+++ profile/src/components/previous.jsx
@@ -1,11 +1,13 @@
 import Button from "@restart/ui/esm/Button";
 import React from "react";
-// import { FiArrowLeft } from "react-icons/fi";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Prev(props){
     return(
-        <Button onClick={props.toggle} disabled={props.active} className="btn"><FontAwesomeIcon icon="FaAngleLeft" /></Button>
+        <Button onClick={props.toggle} disabled={props.active} className="btn">
+            <i className="fa fa-angle-left" style={{backgroundColor:""}}></i>
+        </Button>
+
+            
     )
 }
 
