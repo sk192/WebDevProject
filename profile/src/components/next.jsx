f30b6d3019cc67720339f2f5cdc2ab29102ccec0
sk192 <korulkar.shweta7@gmail.com>
2021-11-30 01:30:19 -0600

    formatted the code

--- profile/src/components/next.jsx
+++ profile/src/components/next.jsx
@@ -1,14 +1,12 @@
 import Button from "@restart/ui/esm/Button";
 import React from "react";
 
-function Next(props){
-    return(
-        <Button onClick={props.toggle} disabled={props.active} className="btn1">
-            <i class="fa fa-angle-right"></i>
-            
-        </Button>
-
-    );
+function Next(props) {
+  return (
+    <Button onClick={props.toggle} disabled={props.active} className="btn1">
+      <i class="fa fa-angle-right"></i>
+    </Button>
+  );
 }
 
 export default Next;
