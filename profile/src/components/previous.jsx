7d791ceb4dc3a78bd577bd980564d65b94544da3
sk192 <korulkar.shweta7@gmail.com>
2021-11-30 01:30:37 -0600

    formatted the code

--- profile/src/components/previous.jsx
+++ profile/src/components/previous.jsx
@@ -1,14 +1,12 @@
 import Button from "@restart/ui/esm/Button";
 import React from "react";
 
-function Prev(props){
-    return(
-        <Button onClick={props.toggle} disabled={props.active} className="btn">
-            <i className="fa fa-angle-left" style={{backgroundColor:""}}></i>
-        </Button>
-
-            
-    )
+function Prev(props) {
+  return (
+    <Button onClick={props.toggle} disabled={props.active} className="btn">
+      <i className="fa fa-angle-left" style={{ backgroundColor: "" }}></i>
+    </Button>
+  );
 }
 
 export default Prev;
