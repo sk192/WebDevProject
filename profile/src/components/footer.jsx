0c7ee7a224eec57ba6e8f10d980e0d32b6f19c0c
sk192 <korulkar.shweta7@gmail.com>
2021-11-30 01:27:25 -0600

    added content to footer

--- profile/src/components/footer.jsx
+++ profile/src/components/footer.jsx
@@ -2,11 +2,53 @@ import React from "react";
 
 const footer = () => {
   return (
-    <footer>
-      <div className="footer">
-        <p>React library</p>
+   <footer className="footer bg-dark">
+      <div className="container">
+        <div className="row">
+          <div className="col col1 ">
+            <h4>PORTFOLIO</h4>
+            {/* <h5 className="list-unstyled">By:</h5> */}
+            <p>
+              <b>
+                <em>By: Shweta Korulkar</em>
+              </b>
+              
+            </p>
+          </div>
+          <div className="col col2">
+            <h4>Powered By</h4>
+            <ul className="list-unstyled">
+              <li>
+                <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                  <b>ReactJS</b>
+                </a>
+              </li>
+            
+              <li>
+                <a
+                  href="https://getbootstrap.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <b>Bootstrap</b>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://fontawesome.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <b>Font Awesome</b>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        
       </div>
     </footer>
+ 
   );
 };
 
