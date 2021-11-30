5a0811b25ad381836c85a1b85e4646ac50835a8c
sk192 <korulkar.shweta7@gmail.com>
2021-11-30 01:30:02 -0600

    formatted the code

--- profile/src/components/contact.jsx
+++ profile/src/components/contact.jsx
@@ -1,20 +1,22 @@
-
 import pic2 from "../images/pic2.jpg";
 
 export default function Contact() {
   return (
     <div className="about-body" role="contentinfo">
       <div className="aboutCard about1">
-        <img src={pic2} alt="shwetha-Avatar" className="aboutImg" width="300px" height = "300px"  />
+        <img
+          src={pic2}
+          alt="shwetha-Avatar"
+          className="aboutImg"
+          width="300px"
+          height="300px"
+        />
         <div className="aboutContainer">
           <h4 className="about-name">
             <b>Shweta Korulkar</b>
           </h4>
-    
-          <a
-            href="https://github.com/sk192"
-            className="about-gitrepo-link"
-          >
+
+          <a href="https://github.com/sk192" className="about-gitrepo-link">
             GitHub{" "}
             <span className="gitExternalLinkIcon">
               <i className="fa fa-external-link-alt gitExternalLink"></i>
@@ -23,5 +25,5 @@ export default function Contact() {
         </div>
       </div>
     </div>
-  
-  )}
+  );
+}
