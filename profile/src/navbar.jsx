de23d21bdfab39eebbd9ab32a559d657956a8316
sk192 <korulkar.shweta7@gmail.com>
2021-11-30 01:32:44 -0600

    added toggle button and formatted the code

--- profile/src/navbar.jsx
+++ profile/src/navbar.jsx
@@ -1,33 +1,34 @@
 import { React } from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function navbar() {
   return (
     <div className="navBar" role="navigation">
-      <Navbar collapseOnSelect bg="light" expand="lg" variant="dark" fixed="top">
-        <Navbar.Brand href="#">
-          <img
-            alt=""
-            src="/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Portfolio
-        </Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="link1" style={{width: "100%"}}>
-            {/* <Nav.Link href="/" className="links">Home</Nav.Link> */}
-            <Nav.Link href="/" className="links">About</Nav.Link>
-            <Nav.Link href="/projects" className="links">Projects</Nav.Link>
-            <Nav.Link href="/resume" className="links">Resume</Nav.Link>
-            <Nav.Link href="/contact" className="links">Contact</Nav.Link>
-          
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg="light" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">
+            <i class="fa fa-user-circle"></i> Portfolio
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="link1 me-auto">
+              <Nav.Link href="/" className="links">
+                About
+              </Nav.Link>
+              <Nav.Link href="/projects" className="links">
+                Projects
+              </Nav.Link>
+              <Nav.Link href="/resume" className="links">
+                Resume
+              </Nav.Link>
+              <Nav.Link href="/contact" className="links">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
-    </div>
+   </div>
   );
 }
 
