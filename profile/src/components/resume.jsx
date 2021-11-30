41284eea375b4c219e70964787f013f862f2e7e1
sk192 <korulkar.shweta7@gmail.com>
2021-11-30 01:31:45 -0600

    removed unnecessary code and formatted the code

--- profile/src/components/resume.jsx
+++ profile/src/components/resume.jsx
@@ -1,39 +1,30 @@
-
 import React, { useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 // import { pdfjs } from "react-pdf/dist/esm/entry.webpack";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import '../stylesheet/resume.css';
-
+import "../stylesheet/resume.css";
 
 export default function SinglePage(props) {
-  
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
- 
+
   function onDocumnetLoadSuccess({ numPages }) {
     setNumPages(numPages);
     setPageNumber(1);
   }
 
-
   return (
     <>
       <Document
-     
         file="./shweta_korulkar_resume.pdf"
-        // file={pdf}
-        // options={{ workerSrc: "/pdf/worker.js" }}
         onLoadSuccess={onDocumnetLoadSuccess}
-        
       >
-        <Page pageNumber={pageNumber}/>
+        <Page pageNumber={pageNumber} />
       </Document>
       <div>
         <p>
           Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
         </p>
-
       </div>
     </>
   );
