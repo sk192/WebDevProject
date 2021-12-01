87f9fbde3bb26b8049c19137025fd657a0455cab
sk192 <korulkar.shweta7@gmail.com>
2021-12-01 01:45:36 -0600

    added hashRouter instead of BrowserRouter to resolve deploy site issue

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./navbar.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {HashRouter  as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/footer';
 import About from "./components/about.jsx";
 import Contact from "./components/contact.jsx";
